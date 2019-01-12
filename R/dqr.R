25e3a6c778a6ac21374d93bdc7498c035624d0c7
Mick Mioduszewski <mick@mioduszewski.net>
2019-01-13 10:14:52 +1100

    initial stub

---
+++ R/dqr.R
@@ -0,0 +1,22 @@
+#' @details
+#' This package will 
+#' ease the pain of assessing and reporting data quality for a dataframe
+#' or a set of them
+#' @keywords internal
+"_PACKAGE"
+
+
+
+#' Report DQ
+#'
+#' \code{dq_rpt(x)} returns the quality data.frame.
+#' @param DF A data.frame.
+#' @return TBD
+#' @examples
+#' x <- data.frame(a = letters[1:9], b = LETTERS[1:9], c = c(1:9))
+#' dq_rpt(x)
+#' @export
+dq_rpt <- function(DF) {
+  nrow(DF)
+}
+
---
+++ tests/testthat/test_period_dates.R
@@ -0,0 +1,7 @@
+context("Data quality for R stub")
+library(lubridate)
+test_that("stub", {
+  expect_equal(TRUE, TRUE)
+})
+
+
